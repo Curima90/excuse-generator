a9adb226aa35cf27fb158e1714622c65fdf6c860
Curima90 <122744151+Curima90@users.noreply.github.com>
2024-04-29 12:51:53 +0000

    exercise done

--- src/app.js
+++ src/app.js
@@ -6,7 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  function frase() {
+    let who = ["the dog", "my granma", "his turtle", "my bird"];
+    let what = ["eat", "pissed", "crushed", "broked"];
+    let when = [
+      "before the class",
+      "right in time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let posicion = Math.floor(Math.random() * 4);
+
+    return who[posicion] + " " + what[posicion] + " " + when[posicion];
+  }
+
+  document.querySelector("#excuse").innerHTML = "Mi excusa:  " + frase();
+
   console.log("Hello Rigo from the console!");
 };
-
